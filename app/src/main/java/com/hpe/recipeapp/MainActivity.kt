93c1520e098da664c0731a34c2baf1ab4d1954d2
Jun Zhang <jun.zhang4@hpe.com>
2021-12-06 12:24:56 +0800

    Hilt Dependency Injection

--- app/src/main/java/com/hpe/recipeapp/MainActivity.kt
+++ app/src/main/java/com/hpe/recipeapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.hpe.recipeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var app: RecipeApplication
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("onCreate: the app context: ${app}")
     }
 }
---
+++ app/src/main/java/com/hpe/recipeapp/RecipeApplication.kt
@@ -0,0 +1,9 @@
+package com.hpe.recipeapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RecipeApplication : Application() {
+
+}
