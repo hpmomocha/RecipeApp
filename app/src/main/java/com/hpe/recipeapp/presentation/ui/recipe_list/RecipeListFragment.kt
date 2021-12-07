b33c6f5a88f6dd38658907021b2eb96021c1a5ff
Jun Zhang <jun.zhang4@hpe.com>
2021-12-07 16:41:40 +0800

    Capturing User Input

--- app/src/main/java/com/hpe/recipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/hpe/recipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.TextField
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.hpe.recipeapp.presentation.components.RecipeCard
@@ -30,11 +36,22 @@ class RecipeListFragment : Fragment() {
 
                 val recipes = viewModel.recipes.value
 
-                LazyColumn {
-                    itemsIndexed(
-                        items = recipes
-                    ) { index, recipe ->
-                        RecipeCard(recipe = recipe, onClick = {})
+                val query = viewModel.query.value
+                Column() {
+                    TextField(
+                        value = query,
+                        onValueChange = {
+                            viewModel.onQueryChanged(it)
+                        })
+
+                    Spacer(modifier = Modifier.padding(10.dp))
+
+                    LazyColumn {
+                        itemsIndexed(
+                            items = recipes
+                        ) { index, recipe ->
+                            RecipeCard(recipe = recipe, onClick = {})
+                        }
                     }
                 }
             }
--- app/src/main/java/com/hpe/recipeapp/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/hpe/recipeapp/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -1,18 +1,12 @@
 package com.hpe.recipeapp.presentation.ui.recipe_list
 
-import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hpe.recipeapp.domain.model.Recipe
 import com.hpe.recipeapp.repository.RecipeRepository
-import com.hpe.recipeapp.util.TAG
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Named
@@ -23,6 +17,8 @@ class RecipeListViewModel @Inject constructor(
     @Named("auth_token") private val token: String,
 ) : ViewModel() {
     val recipes: MutableState<List<Recipe>> = mutableStateOf(ArrayList())
+
+    val query = mutableStateOf("Chicken")
 
     init {
         newSearch()
@@ -37,5 +33,9 @@ class RecipeListViewModel @Inject constructor(
             )
             recipes.value = result
         }
+    }
+
+    fun onQueryChanged(query: String) {
+        this.query.value = query
     }
 }
